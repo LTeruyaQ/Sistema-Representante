0c9499bdf3e8d0565dbd14158902ea74f0b98011
Leandro Teruya de Queiroz <LTeruya.Queiroz@gmail.com>
2022-07-30 22:40:22 -0300

    ajustes no component

--- components/header/header.js
+++ components/header/header.js
@@ -86,8 +86,8 @@ class Header extends HTMLElement {
               </div>
           </div>
       </nav>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
       </header>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
       `
     }
 }
